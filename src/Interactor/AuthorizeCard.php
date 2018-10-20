c889d1931d86f196db3036b4edabf132fda8c8cf
iampersistent <shank@zestic.com>
2018-10-19 21:26:03 -0700

    authorize card

---
+++ src/Interactor/AuthorizeCard.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace IamPersistent\SimpleShop\Interactor;
+
+use IamPersistent\SimpleShop\Entity\CreditCard;
+use IamPersistent\SimpleShop\Interactor\DBal\InsertCard;
+use Omnipay\Common\CreditCard as OmniCreditCard;
+use Omnipay\Common\GatewayInterface;
+
+final class AuthorizeCard
+{
+    private $gateway;
+    private $insertCard;
+
+    public function __construct(GatewayInterface $gateway, InsertCard $insertCard)
+    {
+        $this->gateway = $gateway;
+        $this->insertCard = $insertCard;
+    }
+
+    public function handle(OmniCreditCard $omniCreditCard)
+    {
+        $options = [
+            'card' => $omniCreditCard,
+        ];
+        $response = $this->gateway->createCard($options)->send();
+
+        if ($response->isSuccessful()) {
+            $creditCard = (new CreditCard())
+                ->setCardReference($response->getCardReference())
+                ->setCardNumber($omniCreditCard->getNumberMasked());
+            $this->insertCard->insert($creditCard);
+        }
+    }
+}
