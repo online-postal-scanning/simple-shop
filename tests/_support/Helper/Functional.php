71366822f3100d9fe1f81b370688efb9753319de
Tarun Jangra <tarun@izap.in>
2021-03-23 22:49:23 -0700

    update sqlite driver and replace the deprecated one

--- tests/_support/Helper/Functional.php
+++ tests/_support/Helper/Functional.php
@@ -4,7 +4,7 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Driver\PDOSqlite\Driver;
+use Doctrine\DBAL\Driver\PDO\SQLite\Driver;
 use PDO;
 use Phinx\Config\Config;
 use Phinx\Migration\Manager;
